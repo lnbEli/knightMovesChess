b210f77d23414028a4f60ddad2dd3ce6968549dc
lnbEli <137424458+lnbEli@users.noreply.github.com>
2024-09-15 22:06:38 +0200

    Refactored

--- knightMoves.js
+++ knightMoves.js
@@ -1,44 +1,40 @@
-//Rule of the board
-//Knight can not leave board 8 square x 8 square. Matrix 0-8 x 0-8.
-//Square -1 or 9+ doesnt exist
-//It's basic move is two steps forward and one step to the side or one step forward and two steps to the side.
-//It can face any direction.
-//For every square there is a number of possible moves. What data structure allows to work with them? Graph
-//Which search algorythm to find moves? Potentially infinite series?
-//Find shortest path between node/square and ending square
-//Output full path
+export default function knightMoves(startSquare, endSquare) {
+  //Create board
+  const board = buildBoardWithCoords();
+  //Create adjacency list for board
+  const adjacencyList = createAdjacencyList(board);
 
-export default function knightMoves() {
-  //First build board with square number and its coord
+  //Build board matrix
   function buildBoardWithCoords() {
-    //Number each square between 0- 63
     const board = [];
-    //Create coords for each numbered square
+    //Number each square between 0- 63 a coordinate
     for (let i = 0; i < 8; i++) {
-      for (let j = 0; j < 8; j++) board.push(JSON.stringify([j, i]));
+      for (let j = 0; j < 8; j++) {
+        //Stringify make coords easier to search
+        board.push(JSON.stringify([j, i]));
+      }
     }
     return board;
   }
 
-  //function to build adjacency list for 64 squares
-  function createAdjacencyList(boardCoords) {
+  //function to build adjacency list for all 64 squares of the board
+  function createAdjacencyList(board) {
     const adjacencyList = [];
-    boardCoords.forEach((coord, index, array) => {
+    board.forEach((coord, index, array) => {
       const x = JSON.parse(coord)[0];
       const y = JSON.parse(coord)[1];
-      //create adjacencyList entry
+      //Create adjacencyList entry for each square
       adjacencyList[index] = {
         neighbours: [],
         coord: [x, y],
       };
-
-      //Does board coords include possible move coord
-      //Check upRight Two up one right
-      //Easier to search if array is in string form
+      //Following checks if possible moves have coresponing squares in board
+      //If they exist they are added to adjacency list
+      //Check upRight
       if (array.includes(`[${x + 1},${y + 2}]`)) {
         adjacencyList[index].neighbours.push([x + 1, y + 2]);
       }
-      //check upleft
+      //check upLeft
       if (array.includes(`[${x - 1},${y + 2}]`)) {
         adjacencyList[index].neighbours.push([x - 1, y + 2]);
       }
@@ -71,27 +67,49 @@ export default function knightMoves() {
     return adjacencyList;
   }
 
-  const boardOfCoords = buildBoardWithCoords();
-  const adjacencyList = createAdjacencyList(boardOfCoords);
-
-  //Returns neighbours of Coord. Used to navigate adjacencyList.
-  function returnNeighboursOfCoords(coords) {
-    const x = coords[0];
-    const y = coords[1];
-    let neighbours;
-    adjacencyList.forEach((element) => {
-      if (element.coord[0] === x && element.coord[1] === y) {
-        neighbours = [...element.neighbours];
-      }
-    });
-    return neighbours;
+  //Returns neighbours of coord/square.
+  //Used to navigate adjacencyList.
+  function returnNeighboursOfCoords(coords, adjacencyList) {
+    const [x, y] = coords;
+    const foundElement = adjacencyList.find(
+      (obj) => obj.coord[0] === x && obj.coord[1] === y
+    );
+    return foundElement ? [...foundElement.neighbours] : null;
   }
 
-  return {
-    adjacencyList,
-    boardOfCoords,
-    returnNeighboursOfCoords,
-  };
-}
+  //Returns least amount steps to get from one square to another.
+  //Breadth first BFS is better for shortest path.
+  function searchStepsToCoord(start, end, queue = []) {
+    //Base case. If start coords equal end coords return objects previousSteps Array
+    if (start.coord[0] === end[0] && start.coord[1] === end[1]) {
+      //Adds final step to previous steps array
+      start.previousSteps.push(end);
+      return start.previousSteps;
+    }
+    //Create array of Neighbours for start coord/square
+    const arrayOfNeighbours = returnNeighboursOfCoords(
+      [start.coord[0], start.coord[1]],
+      adjacencyList
+    );
 
-//Function with algo to search fasted possible root from square/node/coord to other. Breath first BFS is better for shortest path
+    //Iterate through all neighbours adding to object with theit details to queue.
+    arrayOfNeighbours.forEach((neighbour) => {
+      //Create array with previous steps(coords) taken to get to this point.
+      const updatedSteps = [...start.previousSteps];
+      //Add current coord/square to array.
+      updatedSteps.push([start.coord[0], start.coord[1]]);
+      //Creat object to be added to queue
+      const obj = { coord: neighbour, previousSteps: updatedSteps };
+      queue.push(obj);
+    });
+    //Remove item from queue
+    const newStartObject = queue.shift();
+    //Recursive call with new start obj/coords
+    return searchStepsToCoord(newStartObject, end, queue);
+  }
+
+  return searchStepsToCoord(
+    { coord: startSquare, previousSteps: [] },
+    endSquare
+  );
+}
--- test.js
+++ test.js
@@ -1,34 +1,3 @@
 import knightMoves from "./knightMoves.js";
 
-function searchStepsToCoord(start, end, queue = []) {
-  if (Array.isArray(start)) {
-    start = { coord: start, previousSteps: [] };
-  }
-
-  //Base case. If start coords equal end coords return objects previousSteps Array
-  if (start.coord[0] === end[0] && start.coord[1] === end[1]) {
-    //Adds final step to previous steps array
-    start.previousSteps.push(end);
-    return start.previousSteps;
-  }
-
-  const arrayOfNeighbours = knightMoves().returnNeighboursOfCoords([
-    start.coord[0],
-    start.coord[1],
-  ]);
-
-  arrayOfNeighbours.forEach((neighbour) => {
-    const previousSteps = [...start.previousSteps];
-    previousSteps.push([start.coord[0], start.coord[1]]);
-    const obj = { coord: neighbour, previousSteps: previousSteps };
-    queue.push(obj);
-  });
-
-  const newStartCoord = queue.shift();
-
-  return searchStepsToCoord(newStartCoord, end, queue);
-}
-
-const arrayOfResults = searchStepsToCoord([4, 3], [2, 5]);
-
-console.log(arrayOfResults);
+console.log(knightMoves([7, 7], [7, 1]));
